db4cb368dc8ab60658482b6f8c3d562663cb040d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-01-07 20:25:24 -0500

    [add] track issues function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import asyncio
+
+# Functions
+from github_bot import get_issues
 
 # Used for retrieving BOT_KEY from .env
 from decouple import config
@@ -11,9 +15,34 @@ BOT_KEY = config('BOT_KEY')
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
 @bot.event
 async def on_ready():
   print(f"{bot.user} has connected to Discord!")
+
+
+CHANNEL_ID = "temporary-channel-id"
+TRACKED_REPO = "GabeDiniz/github_bot"   # repo path (i.e., username/repo)
+
+tracked_issues = set()
+
+async def track_issues():
+  await bot.wait_until_ready()
+  channel = bot.get_channel(CHANNEL_ID)
+  if not channel:
+    print("Invalid channel ID")
+    return
+
+  while True:
+    issues = get_issues(TRACKED_REPO)
+    new_issues = [issue for issue in issues if issue["id"] not in tracked_issues]
+
+    for issue in new_issues:
+      tracked_issues.add(issue["id"])
+      await channel.send(f"New issue created: {issue['title']} - {issue['html_url']}")
+
+    await asyncio.sleep(60)  # Check every minute
+
+bot.loop.create_task(track_issues())
+
 
 bot.run(BOT_KEY)
