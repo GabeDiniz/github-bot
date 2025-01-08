b7002637a6fa78f3a0dc6bc633c9fbc40061de36
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2025-01-07 19:31:56 -0500

    [init] main.py and gitignore

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+# Used for retrieving BOT_KEY from .env
+from decouple import config
+
+# Fetch Credentials from local .env variables 
+# Constants
+BOT_KEY = config('BOT_KEY')
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+  print(f"{bot.user} has connected to Discord!")
+
+bot.run(BOT_KEY)
